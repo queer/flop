5fb34c656dbe0ea6ff96ec8fafd7784651514467
amy null <null@amy.gg>
2023-06-15 23:09:14 -0400

    ✨ feature: prelude!

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
-pub mod prelude {}
+pub mod prelude {
+    pub mod ar {
+        pub use crate::ar::*;
+    }
+    pub mod cpio {
+        pub use crate::cpio::*;
+    }
+    pub mod tar {
+        pub use crate::tar::*;
+    }
+    pub mod zip {
+        pub use crate::zip::*;
+    }
+}
+
 pub mod ar;
 pub mod cpio;
 pub mod tar;
